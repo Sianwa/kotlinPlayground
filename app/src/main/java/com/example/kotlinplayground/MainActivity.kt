4c7fd6dc343dc9d42487995241a399eefa247408
kelsey.makale <kelsey.makale@interswitchgroup.com>
2025-03-18 12:16:40 +0300

    Update: 1. Added performant lazy list. LazyColumn doesn't recycle its children like RecyclerView. It emits new Composables as you scroll through it and is still performant, as emitting Composables is relatively cheap compared to instantiating Android Views.
    2. Added 'rememberSaveable' for state management on orientation change.
    
    3. Added animation to the expandable list item. Weird bug caused the app to crash since it would retract back to dp 0, but the bouncing animation would result in negative padding. So I updated the min dp to 10dp on collapse.
    
    4. Added color and text theming.

--- app/src/main/java/com/example/kotlinplayground/MainActivity.kt
+++ app/src/main/java/com/example/kotlinplayground/MainActivity.kt
@@ -3,6 +3,10 @@ package com.example.kotlinplayground
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -14,20 +18,32 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.kotlinplayground.ui.theme.KotlinPlaygroundTheme
 
 class MainActivity : ComponentActivity() {
@@ -50,19 +66,29 @@ fun Onboarding(modifier: Modifier = Modifier, onClickContinue: () -> Unit){
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text("Welcome to the Basics Codelab!")
+        Text("Welcome to the Basics Codelab!",
+            style = TextStyle(
+                fontFamily = FontFamily.Monospace,
+                fontWeight = FontWeight.SemiBold,
+                fontSize = 16.sp
+            )
+        )
         Button(
             modifier = Modifier.padding(vertical = 24.dp),
             onClick = onClickContinue
         ) {
-            Text("Continue")
+            Text("Continue", style = TextStyle(
+                fontFamily = FontFamily.Monospace,
+                fontWeight = FontWeight.Normal,
+                fontSize = 12.sp))
         }
     }
 }
 
 @Composable
 fun MyApp(modifier: Modifier = Modifier){
-    var shouldShowOnboarding by remember { mutableStateOf(true) }
+    //Instead of using remember you can use rememberSaveable. This will save each state surviving configuration changes (such as rotations) and process death.
+    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
 
     Surface(modifier){
         if(shouldShowOnboarding){
@@ -84,27 +110,57 @@ fun Greetings(modifier: Modifier = Modifier,names: List<String> = List(100){"$it
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    var expanded by remember { mutableStateOf(false) }
-    val extraPadding = if(expanded) 48.dp else 0.dp
-
-    Surface(color = MaterialTheme.colorScheme.primary,
+    Card (colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.primary),
         modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)) {
-        Column(modifier = modifier
-            .fillMaxWidth()
-            .padding(top = 24.dp, bottom = extraPadding)) {
-            Row(horizontalArrangement = Arrangement.SpaceEvenly, modifier = Modifier.fillMaxWidth()){
-                Text(
-                    text = "Welcome to $name!",
+        GreetingCardContent(name = name)
+    }
+}
+
+@Composable
+fun GreetingCardContent(name: String){
+    var expanded by rememberSaveable { mutableStateOf(false) }
+
+    Row(modifier = Modifier
+        .padding(24.dp)
+        .animateContentSize(
+            animationSpec =
+            spring(
+                dampingRatio = Spring.DampingRatioMediumBouncy,
+                stiffness = Spring.StiffnessLow
+            )
+        )) {
+        Column(modifier = Modifier
+            .weight(1f)
+            .padding(bottom = 12.dp)
+        ) {
+            Text(text = "Hello, ",
+                style = TextStyle(
+                    fontFamily = FontFamily.Monospace,
+                    fontWeight = FontWeight.Medium,
+                    fontSize = 12.sp))
+            Text(text = name,
+                style = MaterialTheme.typography.headlineMedium.copy(
+                    fontFamily = FontFamily.Monospace
                 )
-                Spacer(modifier = modifier.width(24.dp))
-                ElevatedButton(onClick = {
-                    expanded = !expanded;
-                }) {
-                    Text(if(expanded) "Show Less" else "Show More")
-                }
+            )
+            if(expanded){
+                Text(text = "We are happy to have you here....",
+                    style = TextStyle(fontFamily = FontFamily.Monospace)
+                )
             }
         }
+        IconButton(
+            onClick = { expanded = !expanded }
+        ) {
+            Icon(imageVector = if (expanded) Icons.Filled.KeyboardArrowUp else Icons.Filled.KeyboardArrowDown,
+                contentDescription = if (expanded) {
+                    "Show Less"
+                } else {
+                    "Show More"
+                })
+        }
     }
+
 }
 
 @Preview(showBackground = true)
